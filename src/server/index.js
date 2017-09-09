450a90845667b276c4eea86581d5dcc664c0d5b4
Gethyn Jones <gethyn1@gmail.com>
2017-09-09 13:01:44 +0100

    Only apply API rate limiting in production

--- src/server/index.js
+++ src/server/index.js
@@ -36,16 +36,18 @@ if (!isProd && DEBUG) {
 app.use(helmet())
 
 // Rate limiting
-app.enable('trust proxy')
+if (isProd) {
+  app.enable('trust proxy')
 
-const limiter = new RateLimit({
-  windowMs: 15*60*1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-  delayMs: 0, // disable delaying - full speed until the max limit is reached
-})
+  const limiter = new RateLimit({
+    windowMs: 15*60*1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    delayMs: 0, // disable delaying - full speed until the max limit is reached
+  })
 
-//  apply to all requests
-app.use(limiter);
+  //  apply to all requests
+  app.use(limiter)
+}
 
 // Setup CORS so front-end app can access the API
 app.all('*', function(req, res, next) {
